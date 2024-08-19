0da3f3187d2b2179851e6013cb4f262f94702564
Andrey <andreyrcdias@gmail.com>
2024-08-19 19:45:13 -0300

    improving repl and main

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/andreyrcdias/monkey/repl"
 	"os"
 )
 
 func main() {
-	// TODO: how to proper version a golang package
-	version := "0.0.1"
-	fmt.Printf("Monkey Language %s\n", version)
-	// TODO: implement help, credits commands
-	// fmt.Printf("Type \"help\", \"credits\" for more information.\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -8,21 +8,45 @@ import (
 	"io"
 )
 
+const MONKEY_LOGO = `
+               o
+               | /
+o-O-o o-o o-o  OO   o-o o  o
+| | | | | |  | | \  |-' |  |
+o o o o-o o  o o  o o-o o--O
+                           |
+                        o--o
+`
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
+	// TODO: how to proper version a golang package
+	version := "0.0.1"
+
+	fmt.Print(MONKEY_LOGO)
+	fmt.Printf("Monkey Language %s\n", version)
+
+	fmt.Printf("Type \"help\", \"credits\" for more information.\n")
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 		line := scanner.Text()
 
-		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		switch line {
+		case "help":
+			fmt.Printf("TODO")
+		case "credits":
+			fmt.Printf(" Thanks Thorsten Ball for this Amazin book. \n")
+		default:
+			l := lexer.New(line)
+			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+				fmt.Printf("%+v\n", tok)
+			}
 		}
+
 	}
 }
